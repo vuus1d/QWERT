26cc0bf0455027389998030fdd7cc48cdf2d2d4f
honor <honornurasyl@gmail.com>
2025-12-19 12:26:39 +0500

    next comit
    Signed-off-by: honor <honornurasyl@gmail.com>

--- Main.java
+++ Main.java
@@ -4,20 +4,25 @@ public class Main {
 
         Bus bus1 = new Bus(10, 50);
         Bus bus2 = new Bus(20, 40);
-
+        Bus bus3 = new Bus(30, 60);
         Commuter commuter = new Commuter("Nurasyl", 500);
 
         TransportService service = new TransportService("City Transport", 120);
 
         bus1.printInfo();
         bus2.printInfo();
+        bus3.printInfo();
         commuter.printInfo();
         service.printInfo();
 
         // сравнение автобусов
-        if (bus1.getCapacity() > bus2.getCapacity()) {
+        if (bus1.getCapacity() > bus2.getCapacity() && bus1.getCapacity()> bus3.getCapacity()) {
             System.out.println("Bus 1 has larger capacity");
-        } else {
+        }else if (bus3.getCapacity()> bus1.getCapacity()){
+            System.out.println("Bus 3 has larger that 1");
+        }
+
+        else {
             System.out.println("Bus 2 has larger or equal capacity");
         }
     }
